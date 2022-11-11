f4a625f80ecb14fa6e4c702d2b186e75dcb5e3c5
Jenn Yacoubian <jyacoubian@gmail.com>
2022-11-11 15:09:15 -0700

    Reformat data

--- src/sample-data.js
+++ src/sample-data.js
@@ -23,28 +23,28 @@ const sampleCustomers = [
 
 const sampleRooms = [
     {
-    number: 2,
-    roomType: "suite",
-    bidet: false,
-    bedSize: "full",
-    numBeds: 2,
-    costPerNight: 477.38
+        number: 2,
+        roomType: "suite",
+        bidet: false,
+        bedSize: "full",
+        numBeds: 2,
+        costPerNight: 477.38
     },
     {
-    number: 3,
-    roomType: "single room",
-    bidet: false,
-    bedSize: "king",
-    numBeds: 1,
-    costPerNight: 491.14
+        number: 3,
+        roomType: "single room",
+        bidet: false,
+        bedSize: "king",
+        numBeds: 1,
+        costPerNight: 491.14
     },
     {
-    number: 4,
-    roomType: "single room",
-    bidet: false,
-    bedSize: "queen",
-    numBeds: 1,
-    costPerNight: 429.44
+        number: 4,
+        roomType: "single room",
+        bidet: false,
+        bedSize: "queen",
+        numBeds: 1,
+        costPerNight: 429.44
     },
     {
         number: 6,
