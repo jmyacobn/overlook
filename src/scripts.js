5f1deaecc1079486ec781c836d921db68a8c9bc0
Jenn Yacoubian <jyacoubian@gmail.com>
2022-11-11 20:28:07 -0700

    Add import, event listener, global variables, and function for fetch as well as organizational comments

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,28 @@
-import './css/styles.css';
+// ~~~~~~~~~~~~~~~~~~~~Imports~~~~~~~~~~~~~~~~~~~~
+import './css/styles.css'
 import './images/5-stars.png'
+import getData from './apiCalls'
+
+// ~~~~~~~~~~~~~~~~~~~~Global Variables~~~~~~~~~~~~~~~~~~~~
+let allCustomers
+let allRooms
+let allBookings
+
+// ~~~~~~~~~~~~~~~~~~~~Query Selectors~~~~~~~~~~~~~~~~~~~~
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// ~~~~~~~~~~~~~~~~~~~~Event Listeners~~~~~~~~~~~~~~~~~~~~
+window.addEventListener('load', fetchData())
+
+
+// ~~~~~~~~~~~~~~~~~~~~Functions~~~~~~~~~~~~~~~~~~~~
+function fetchData() {
+    Promise.all([getData('customers'), getData('rooms'), getData('bookings')])
+    .then(data => {
+        allCustomers = data[0]
+        allRooms = data[1]
+        allBookings = data[2]
+    })
+}
+
+// ~~~~~~~~~~~~~~~~~~~~Helper Functions~~~~~~~~~~~~~~~~~~~~
