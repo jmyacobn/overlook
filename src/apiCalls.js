da0cc63b91f479a754629ea10e8ed7eda965793d
Jenn Yacoubian <jyacoubian@gmail.com>
2022-11-15 10:21:53 -0700

    Change double quotes to single quotes in API call file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,13 +2,13 @@ const getData = (dataSource) => {
   return fetch(`http://localhost:3001/api/v1/${dataSource}`)
     .then(response => {
       if (!response.ok) {
-        throw new Error("Sorry, we were unable to retreive your account information. Please try again.")
+        throw new Error('Sorry, we were unable to retreive your account information. Please try again.')
       }
       return response.json()
     })
     .catch(err => {
       console.log(err)
-      availableRooms.innerHTML = "Oops, something went wrong. Please try again"
+      availableRooms.innerHTML = 'Oops, something went wrong. Please try again'
     })
 }
 
@@ -16,13 +16,13 @@ const postData = (newBooking) => {
   return fetch('http://localhost:3001/api/v1/bookings', newBooking)
     .then(response => {
       if (!response.ok) {
-        throw new Error("Sorry, your booking request did not go through. Please try again.")
+        throw new Error('Sorry, your booking request did not go through. Please try again.')
       }
       return response.json()
     })
     .catch(err => {
       console.log(err)
-      availableRooms.innerHTML = "Oops, something went wrong. Please try again"
+      availableRooms.innerHTML = 'Oops, something went wrong. Please try again'
     })
 }
 
